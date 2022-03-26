6c6fb1dd5a8120bbb0aaa80ab9b1689dba8d1561
dali <lionelviperdali@gmail.com>
2022-03-26 17:33:38 +0100

    test: add test for `setFieldValueById`

--- test/dynamicform_test.dart
+++ test/dynamicform_test.dart
@@ -3,6 +3,59 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  testWidgets("test setFieldValue", (tester) async {
+    final controller = FormController();
+    await tester.pumpWidget(MaterialApp(
+      home: Scaffold(
+        body: SimpleDynamicForm(
+          controller: controller,
+          groupElements: [
+            GroupElement(
+              margin: EdgeInsets.only(bottom: 5.0),
+              directionGroup: DirectionGroup.Horizontal,
+              sizeElements: [0.3],
+              textElements: [
+                TextElement(
+                    id: "id_first_name",
+                    initValue: "",
+                    label: "first name",
+                    hint: "first name",
+                    validator: (v) {
+                      if (v!.isEmpty) {
+                        return "err";
+                      }
+                      return null;
+                    }),
+                TextElement(
+                    label: "last name",
+                    isRequired: true,
+                    initValue: "your name",
+                    validator: (v) {
+                      if (v != "your name") {
+                        return "name not accepted";
+                      }
+                      return null;
+                    }),
+              ],
+            ),
+          ],
+        ),
+      ),
+    ));
+    await tester.pumpAndSettle(Duration(seconds: 1));
+     TextFormField firstNameTextField = tester.widget(find.byType(TextFormField).first);
+    final TextFormField lastNameTextField = tester.widget(find.byType(TextFormField).last);
+    expect(firstNameTextField.initialValue?.isEmpty, true);
+    expect(lastNameTextField.initialValue, "your name");
+
+    controller.setFieldValueById("id_first_name", "Mohamed");
+    await tester.pumpAndSettle(Duration(seconds: 1));
+     firstNameTextField = tester.widget(find.byType(TextFormField).first);
+    expect(firstNameTextField.controller!.text, "Mohamed");
+
+  });
+
+
   testWidgets("test password controls login form", (tester) async {
     final controller = LoginFormController();
     await tester.pumpWidget(
@@ -43,8 +96,7 @@ void main() {
     );
     await tester.pump();
 
-    TextFormField password =
-        tester.widget(find.byType(TextFormField).at(1)) as TextFormField;
+    TextFormField password = tester.widget(find.byType(TextFormField).at(1)) as TextFormField;
 
     String? v = password.validator!("da");
     expect(v, "min length is 3");
